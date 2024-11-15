6ccdf0881bdc2137d54754a2401615986730ae77
Gunther0662 <nicholasdiaz17@yahoo.com>
2024-11-14 21:42:39 -0600

    added the changes to the home.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,12 @@
 import './App.css';
 import { Outlet } from 'react-router-dom';
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 import Nav from './components/NavTabs';
+
+const client = new ApolloClient({
+    uri: '/graphl',
+    cache: new InMemoryCache(),
+});
 
 function App() {
 
