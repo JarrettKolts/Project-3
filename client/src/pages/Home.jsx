13a28141885132927dec88360ff9b659150c7856
Gunther0662 <nicholasdiaz17@yahoo.com>
2024-11-18 18:25:03 -0600

    fixed the problem with image

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -28,13 +28,13 @@ return (
         </section>
         <div class="container">
   <div class="row col-xs-2">
-  <img style= "width: 25%; height: 25%" src="images\curry.jpg" alt="Mister Marbles head tilt">
+  <img style= "width: 25%; height: 25%" src="images\curry.jpg" alt="Mister Marbles head tilt"/>
 </div>  
 <div class="row col-xs-2">
-  <img style= "width: 25%; height: 25%"  src="images\Luqaimat.jpg" alt="Kermit the Dog">
+  <img style= "width: 25%; height: 25%"  src="images\Luqaimat.jpg" alt="Kermit the Dog"/>
 </div>
 <div class="row col-xs-2">
-  <img style= "width: 25%; height: 25%" src="images\ramen.jpg" alt="Peach posing magestically">  
+  <img style= "width: 25%; height: 25%" src="images\ramen.jpg" alt="Peach posing magestically"/>  
 </div>  
 </div>
         <section>
@@ -42,7 +42,7 @@ return (
   <div class="row">
     <div class="col-md-4">
       <div class="card">
-        <img src="images\Baked Cod.jpg" class="card-img-top" alt="...">
+        <img src="images\Baked Cod.jpg" class="card-img-top" alt="..."/>
         <div class="card-body">
           <h5 class="card-title">Baked Cod</h5>
           <p class="card-text">This baked cod recipe is simple, fast, and delicious. The fish is baked in butter, the topped with lemon juice, white wine, and buttery crackers for the perfect ten dinner. This dish is a favorite of ours from a local resturant. Serve with rice pilaf and sauteed spinach with garlic.</p>
@@ -52,7 +52,7 @@ return (
     </div>
     <div class="col-md-4">
       <div class="card">
-        <img src="images\chinese-pan-fried-dumplings.jpg" class="card-img-top" alt="...">
+        <img src="images\chinese-pan-fried-dumplings.jpg" class="card-img-top" alt="..."/>
         <div class="card-body">
           <h5 class="card-title">Chinese-pan fried dumplings</h5>
           <p class="card-text">Delicious crispy pan-fried dumplings are a satisfying favorite. Often they are filled with pork, cabbage, shiitake mushrooms, and garlic chives</p>
@@ -62,7 +62,7 @@ return (
     </div>
     <div class="col-md-4">
       <div class="card">
-        <img src="images\Jollof-Rice.webp" class="card-img-top" alt="...">
+        <img src="images\Jollof-Rice.webp" class="card-img-top" alt="..."/>
         <div class="card-body">
           <h5 class="card-title">Jollof Rice</h5>
           <p class="card-text">Jollof Rice, a West-African one-pot dish with rice, tomatoes, peppers, and spices.</p>
